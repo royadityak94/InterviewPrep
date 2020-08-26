a8113874dd10cd2fb57450c7e115c3f3a3f7832b
Aditya Kumar Roy <adityakumar.roy94@gmail.com>
2020-08-25 20:10:37 -0400

    Updated Dev

--- GoodWillHunting/color_code_logger_test.py
+++ GoodWillHunting/color_code_logger_test.py
@@ -1,3 +1,10 @@
-color_list = [*range(30, 37), *range(90, 91)]
-for color in color_list:
-    print ('\033[%dm'%color, 'Hello!', '\033[0m', sep='')
+import ipdb
+from memory_profiler import profile
+
+@profile
+def main():
+    color_list = [*range(30, 37), *range(90, 91)]
+    for color in color_list:
+        print ('\033[%dm'%color, 'Hello!', '\033[0m', sep='')
+
+main()
