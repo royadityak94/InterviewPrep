86d847394c08183aff0191bdc40aa459d0ebdb53
royadityak94 <akroy@umass.edu>
2021-03-01 04:49:18 -0500

    Added Practice codes to Leetcode hard

--- SQL/LeetCode_hard.sql
+++ SQL/LeetCode_hard.sql
@@ -491,23 +491,293 @@ WHERE
     row_id != 1
 ORDER BY Id, Month DESC;
 
-`Problem:
-Task:
+`Problem: Hopper Company Queries I
+Task: Write an SQL query to report the following statistics for each month of 2020:
+The number of drivers currently with the Hopper company by the end of the month (active_drivers). The number of accepted rides in that month (accepted_rides). Return the result table ordered by month in ascending order, where month is the month's number (January is 1, February is 2, etc.).
+Drivers table:
++-----------+------------+
+| driver_id | join_date  |
++-----------+------------+
+| 10        | 2019-12-10 |
+| 8         | 2020-1-13  |
+| 5         | 2020-2-16  |
+| 7         | 2020-3-8   |
+| 4         | 2020-5-17  |
+| 1         | 2020-10-24 |
+| 6         | 2021-1-5   |
++-----------+------------+
+
+Rides table:
++---------+---------+--------------+
+| ride_id | user_id | requested_at |
++---------+---------+--------------+
+| 6       | 75      | 2019-12-9    |
+| 1       | 54      | 2020-2-9     |
+| 10      | 63      | 2020-3-4     |
+| 19      | 39      | 2020-4-6     |
+| 3       | 41      | 2020-6-3     |
+| 13      | 52      | 2020-6-22    |
+| 7       | 69      | 2020-7-16    |
+| 17      | 70      | 2020-8-25    |
+| 20      | 81      | 2020-11-2    |
+| 5       | 57      | 2020-11-9    |
+| 2       | 42      | 2020-12-9    |
+| 11      | 68      | 2021-1-11    |
+| 15      | 32      | 2021-1-17    |
+| 12      | 11      | 2021-1-19    |
+| 14      | 18      | 2021-1-27    |
++---------+---------+--------------+
+
+AcceptedRides table:
++---------+-----------+---------------+---------------+
+| ride_id | driver_id | ride_distance | ride_duration |
++---------+-----------+---------------+---------------+
+| 10      | 10        | 63            | 38            |
+| 13      | 10        | 73            | 96            |
+| 7       | 8         | 100           | 28            |
+| 17      | 7         | 119           | 68            |
+| 20      | 1         | 121           | 92            |
+| 5       | 7         | 42            | 101           |
+| 2       | 4         | 6             | 38            |
+| 11      | 8         | 37            | 43            |
+| 15      | 8         | 108           | 82            |
+| 12      | 8         | 38            | 34            |
+| 14      | 1         | 90            | 74            |
++---------+-----------+---------------+---------------+
+
+Result table:
++-------+----------------+----------------+
+| month | active_drivers | accepted_rides |
++-------+----------------+----------------+
+| 1     | 2              | 0              |
+| 2     | 3              | 0              |
+| 3     | 4              | 1              |
+| 4     | 4              | 0              |
+| 5     | 5              | 0              |
+| 6     | 5              | 1              |
+| 7     | 5              | 1              |
+| 8     | 5              | 1              |
+| 9     | 5              | 0              |
+| 10    | 6              | 0              |
+| 11    | 6              | 2              |
+| 12    | 6              | 1              |
++-------+----------------+----------------+
 `
 
+WITH RECURSIVE months(month) AS (
+    SELECT 1 month
+    UNION ALL
+    SELECT month+1 FROM months WHERE month < 12
+),
+driver_stats AS (
+    SELECT
+        month(join_date) month,
+        COUNT(driver_id) OVER(ORDER BY join_date) active_drivers
+    FROM
+        drivers
+    WHERE
+        year(join_date) <= 2020
+),
+rider_stats AS (
+    SELECT
+        month(requested_at) month,
+        COUNT(ride_id) accepted_rides
+    FROM
+        Rides
+    WHERE
+        ride_id IN (SELECT DISTINCT ride_id FROM AcceptedRides)
+        AND
+        year(requested_at) = 2020
+    GROUP BY
+        month
+)
+SELECT DISTINCT
+    m.month,
+    IFNULL(MAX(ds.active_drivers) OVER(ORDER BY m.month), 0) active_drivers,
+    IFNULL(rs.accepted_rides, 0) accepted_rides
+FROM
+    months m
+    LEFT JOIN driver_stats ds ON (m.month = ds.month)
+    LEFT JOIN rider_stats rs ON (m.month = rs.month)
+;
 
-`Problem:
-Task:
+`Problem: Hopper Company Queries III
+Task: Write an SQL query to compute the average_ride_distance and average_ride_duration of every 3-month window starting from January - March 2020 to October - December 2020. Round average_ride_distance and average_ride_duration to the nearest two decimal places.
+Result table:
++-------+-----------------------+-----------------------+
+| month | average_ride_distance | average_ride_duration |
++-------+-----------------------+-----------------------+
+| 1     | 21.00                 | 12.67                 |
+| 2     | 21.00                 | 12.67                 |
+| 3     | 21.00                 | 12.67                 |
+| 4     | 24.33                 | 32.00                 |
+| 5     | 57.67                 | 41.33                 |
+| 6     | 97.33                 | 64.00                 |
+| 7     | 73.00                 | 32.00                 |
+| 8     | 39.67                 | 22.67                 |
+| 9     | 54.33                 | 64.33                 |
+| 10    | 56.33                 | 77.00                 |
++-------+-----------------------+-----------------------+
+`
+WITH RECURSIVE months(month) AS (
+  SELECT 1
+  UNION ALL
+  SELECT month + 1 FROM months WHERE month < 12
+),
+
+ride_summary AS (
+  SELECT
+    month(r.requested_at) month,
+    SUM(ar.ride_distance) month_ride_distance,
+    SUM(ar.ride_duration) monthly_ride_duration
+  FROM
+    Rides r
+    JOIN AcceptedRides ar
+    ON (r.ride_id = ar.ride_id)
+    WHERE year(r.requested_at) = '2020'
+    GROUP BY month
+)
+
+SELECT
+  m.month month,
+  ROUND(AVG(CAST(IFNULL(rs.month_ride_distance, 0) AS FLOAT)) OVER(ORDER BY month ROWS BETWEEN CURRENT ROW AND 2 FOLLOWING), 2) average_ride_distance,
+  ROUND(AVG(CAST(IFNULL(rs.monthly_ride_duration, 0) AS FLOAT)) OVER(ORDER BY month ROWS BETWEEN CURRENT ROW AND 2 FOLLOWING), 2) average_ride_duration
+FROM
+  months m
+  LEFT JOIN
+  ride_summary rs
+    ON (m.month = rs.month)
+ORDER BY
+  month
+LIMIT 10;
+
+`Problem: Hopper Company Queries II
+Task: Write an SQL query to report the percentage of working drivers (working_percentage) for each month of 2020
+Result table:
++-------+--------------------+
+| month | working_percentage |
++-------+--------------------+
+| 1     | 0.00               |
+| 2     | 0.00               |
+| 3     | 25.00              |
+| 4     | 0.00               |
+| 5     | 0.00               |
+| 6     | 20.00              |
+| 7     | 20.00              |
+| 8     | 20.00              |
+| 9     | 0.00               |
+| 10    | 0.00               |
+| 11    | 33.33              |
+| 12    | 16.67              |
++-------+--------------------+
 `
 
+WITH RECURSIVE months(month) AS (
+  SELECT 1 month
+  UNION ALL
+  SELECT month+1 FROM months WHERE month < 12
+),
 
-`Problem:
-Task:
+driver_infomation AS (
+  SELECT
+    month(join_date) month,
+    count(driver_id) OVER(ORDER BY join_date ROWS UNBOUNDED PRECEDING) active_drivers
+  FROM
+    drivers
+  WHERE
+    year(join_date) <= 2020
+),
+
+available_drivers AS (
+  SELECT
+    m.month,
+    IFNULL(MAX(di.active_drivers) OVER(ORDER BY month), 0) active_drivers
+  FROM
+    months m
+    LEFT JOIN
+    driver_infomation di
+    ON (m.month = di.month)
+),
+
+working_drivers AS (
+  SELECT
+    month(r.requested_at) month,
+    COUNT(DISTINCT driver_id) working_drivers
+  FROM
+    Rides r
+    JOIN AcceptedRides ar
+    ON (r.ride_id = ar.ride_id)
+    WHERE year(r.requested_at) = 2020
+    GROUP BY month
+)
+
+SELECT
+  m.month month,
+  IFNULL(ROUND(working_drivers/active_drivers*100, 2), 0) working_percentage
+FROM
+  months m
+  LEFT JOIN available_drivers ad ON (ad.month = m.month)
+  LEFT JOIN working_drivers wd ON (wd.month = m.month)
+GROUP BY
+  month
+;
+
+`Problem: Find the Subtasks That Did Not Execute
+Task: Write an SQL query to report the IDs of the missing subtasks for each task_id.
+Tasks table:
++---------+----------------+
+| task_id | subtasks_count |
++---------+----------------+
+| 1       | 3              |
+| 2       | 2              |
+| 3       | 4              |
++---------+----------------+
+Executed table:
++---------+------------+
+| task_id | subtask_id |
++---------+------------+
+| 1       | 2          |
+| 3       | 1          |
+| 3       | 2          |
+| 3       | 3          |
+| 3       | 4          |
++---------+------------+
+Result table:
++---------+------------+
+| task_id | subtask_id |
++---------+------------+
+| 1       | 1          |
+| 1       | 3          |
+| 2       | 1          |
+| 2       | 2          |
++---------+------------+
+`
+WITH RECURSIVE task_listing AS (
+  SELECT task_id, 1 subtask_id, subtasks_count FROM Tasks
+  UNION ALL
+  SELECT task_id, subtask_id + 1 AS subtask_id, subtasks_count FROM task_listing
+    WHERE subtask_id < (SELECT MAX(subtasks_count) FROM Tasks)
+)
+
+SELECT
+  tl.task_id task_id,
+  tl.subtask_id subtask_id
+FROM
+  task_listing tl
+  LEFT JOIN Executed e
+  ON (tl.task_id = e.task_id AND tl.subtask_id = e.subtask_id)
+  WHERE
+    e.subtask_id IS NULL
+    AND
+    tl.subtasks_count >= tl.subtask_id
+;
+`
+N.B: We have to do tl.subtasks_count >= tl.subtask_id because earlier we had generated as much
+subtask_id as MAX(subtasks_count). WE could have done: WHERE subtask_id < subtasks_count. However, that would have been
+much more expensive and imfeasible (almost 2x speedup). But, if we did, we do not have to include:
+tl.subtasks_count >= tl.subtask_id in our where clause!!!
 `
 
-`Problem:
-Task:
-`
 
 `Problem:
 Task:
