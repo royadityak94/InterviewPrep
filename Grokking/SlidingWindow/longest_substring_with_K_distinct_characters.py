69b78ffb006cfe98da714462b1cfd385b2167f1d
royadityak94 <akroy@umass.edu>
2023-01-25 23:29:26 -0600

    Updated comments in the development

--- Grokking/SlidingWindow/longest_substring_with_K_distinct_characters.py
+++ Grokking/SlidingWindow/longest_substring_with_K_distinct_characters.py
@@ -1,4 +1,22 @@
 # Program to implement the longest substring with K distinct character (Hashmap Implementation)
+"""
+Problem Statement #
+Given a string, find the length of the longest substring in it with no more than K distinct characters.
+Example 1:
+Input: String="araaci", K=2
+Output: 4
+Explanation: The longest substring with no more than '2' distinct characters is "araa".
+Example 2:
+Input: String="araaci", K=1
+Output: 2
+Explanation: The longest substring with no more than '1' distinct characters is "aa".
+Example 3:
+Input: String="cbbebi", K=3
+Output: 5
+Explanation: The longest substrings with no more than '3' distinct characters are "cbbeb" & "bbebi".
+"""
+
+
 def test(expected, output, msg=''):
     if expected == output:
         print ("Test Case successful: %s" % msg)
